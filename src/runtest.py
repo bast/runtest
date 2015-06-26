da72f55274101d58eff8f3fa58a2f19e6e858bce
Radovan Bast <bast@users.noreply.github.com>
2015-06-26 17:14:13 +0200

    add unit test for extract_numbers

--- src/runtest.py
+++ src/runtest.py
@@ -26,7 +26,7 @@ import string
 from optparse import OptionParser
 
 
-__version__ = '1.3.0'  # http://semver.org
+__version__ = '1.3.1'  # http://semver.org
 
 
 class FilterKeywordError(Exception):
@@ -47,6 +47,116 @@ class AcceptedError(Exception):
 
 class SubprocessError(Exception):
     pass
+
+
+# ------------------------------------------------------------------------------
+
+def test_extract_numbers():
+    """
+    Tests extract_numbers.
+    """
+
+    text = '''<<A( 3),B( 3)>> - linear response function (real):
+-----------------------------------------------------------------------------------------------
+   A - Z-Dipole length      B1u  T+
+   B - Z-Dipole length      B1u  T+
+-----------------------------------------------------------------------------------------------
+ Frequency (real)     Real part                                     Convergence
+-----------------------------------------------------------------------------------------------
+  0.00000000 a.u.   -1.901357604797 a.u.                       3.04E-07   (converged)
+-----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------
+
+
+                         +--------------------------------+
+                         ! Electric dipole polarizability !
+                         +--------------------------------+
+
+
+ 1 a.u =   0.14818471 angstrom**3
+
+
+@   Elements of the electric dipole polarizability tensor
+
+@   xx            1.90135760 a.u.   (converged)
+@   yy            1.90135760 a.u.   (converged)
+@   zz            1.90135760 a.u.   (converged)
+
+@   average       1.90135760 a.u.
+@   anisotropy    0.000      a.u.
+
+@   xx            0.28175212 angstrom**3
+@   yy            0.28175212 angstrom**3
+@   zz            0.28175212 angstrom**3
+
+@   average       0.28175212 angstrom**3
+@   anisotropy    0.000      angstrom**3'''
+
+    f = Filter()
+    f.add()
+
+    numbers, locations = extract_numbers(f.filter_list[0], text.splitlines())
+
+    assert numbers == [0.0, -1.901357604797, 3.04e-07, 1, 0.14818471, 1.9013576, 1.9013576, 1.9013576, 1.9013576, 0.0, 0.28175212, 0.28175212, 0.28175212, 0.28175212, 0.0]
+    assert locations == [(7, 2, 10), (7, 20, 15), (7, 63, 8), (17, 1, 1), (17, 11, 10), (22, 18, 10), (23, 18, 10), (24, 18, 10), (26, 18, 10), (27, 18, 5), (29, 18, 10), (30, 18, 10), (31, 18, 10), (33, 18, 10), (34, 18, 5)]
+
+
+def extract_numbers(f, text):
+    """
+    Input:
+        - f -- filter task
+        - text -- list of lines where we extract numbers from
+
+    Returns:
+        - numbers -- list of numbers
+        - locations -- locations of each number, list of triples
+                      (line, start position, length)
+    """
+
+    numeric_const_pattern = r"""
+    [-+]? # optional sign
+    (?:
+        (?: \d* \. \d+ ) # .1 .12 .123 etc 9.1 etc 98.1 etc
+        |
+        (?: \d+ \.? ) # 1. 12. 123. etc 1 12 123 etc
+    )
+    # followed by optional exponent part if desired
+    (?: [EeDd] [+-]? \d+ ) ?
+    """
+
+    pattern_int = re.compile('^-?[0-9]+$', re.VERBOSE)
+    pattern_float = re.compile(numeric_const_pattern, re.VERBOSE)
+    pattern_d = re.compile(r'[dD]')
+
+    numbers = []
+    locations = []
+
+    for n, line in enumerate(text):
+        i = 0
+        for w in line.split():
+            # do not consider words like TzB1g
+            # otherwise we would extract 1 later
+            if re.match(r'^[0-9\.eEdD\+\-]*$', w):
+                i += 1
+                if (f.use_mask) and (i not in f.mask):
+                    continue
+                is_integer = False
+                if len(pattern_float.findall(w)) > 0:
+                    is_integer = (pattern_float.findall(w) == pattern_int.findall(w))
+                # apply floating point regex
+                for m in pattern_float.findall(w):
+                    index = line.index(m)
+                    # substitute dD by e
+                    m = pattern_d.sub('e', m)
+                    if is_integer:
+                        numbers.append(int(m))
+                    else:
+                        numbers.append(float(m))
+                    locations.append((n, index, len(m)))
+
+    return numbers, locations
+
+# ------------------------------------------------------------------------------
 
 
 class TestRun:
@@ -306,11 +416,15 @@ class Filter:
 
             out_filtered = self._filter_file(f, out_name)
             log_out.write(''.join(out_filtered))
-            out_numbers, out_location = self._extract_numbers(f, out_filtered)
+            out_numbers, out_locations = extract_numbers(f, out_filtered)
+            if f.use_mask and out_numbers == []:
+                raise FilterKeywordError('ERROR: mask %s did not extract any numbers\n' % f.mask)
 
             ref_filtered = self._filter_file(f, ref_name)
             log_ref.write(''.join(ref_filtered))
-            ref_numbers, ref_location = self._extract_numbers(f, ref_filtered)
+            ref_numbers, ref_locations = extract_numbers(f, ref_filtered)
+            if f.use_mask and ref_numbers == []:
+                raise FilterKeywordError('ERROR: mask %s did not extract any numbers\n' % f.mask)
 
             if out_numbers == [] and ref_numbers == []:
                 # no numbers are extracted
@@ -328,7 +442,7 @@ class Filter:
                     for k, line in enumerate(out_filtered):
                         log_diff.write('.       %s' % line)
                         for i, num in enumerate(out_numbers):
-                            (line_num, start_char, length) = out_location[i]
+                            (line_num, start_char, length) = out_locations[i]
                             if line_num == k:
                                 if l[i] == 0:
                                     is_integer = isinstance(num, int)
@@ -404,67 +518,6 @@ class Filter:
             raise BadFilterError(message)
 
         return output_filtered
-
-    def _extract_numbers(self, f, text):
-        """
-        Input:
-            - f -- filter task
-            - text -- list of lines where we extract numbers from
-
-        Returns:
-            - numbers -- list of numbers
-            - location -- location of each number, list of triples
-                          (line, start position, length)
-
-        Raises:
-            - nothing
-        """
-
-        numeric_const_pattern = r"""
-        [-+]? # optional sign
-        (?:
-            (?: \d* \. \d+ ) # .1 .12 .123 etc 9.1 etc 98.1 etc
-            |
-            (?: \d+ \.? ) # 1. 12. 123. etc 1 12 123 etc
-        )
-        # followed by optional exponent part if desired
-        (?: [EeDd] [+-]? \d+ ) ?
-        """
-
-        pattern_int = re.compile('^-?[0-9]+$', re.VERBOSE)
-        pattern_float = re.compile(numeric_const_pattern, re.VERBOSE)
-        pattern_d = re.compile(r'[dD]')
-
-        numbers = []
-        location = []
-
-        for n, line in enumerate(text):
-            i = 0
-            for w in line.split():
-                # do not consider words like TzB1g
-                # otherwise we would extract 1 later
-                if re.match(r'^[0-9\.eEdD\+\-]*$', w):
-                    i += 1
-                    if (f.use_mask) and (i not in f.mask):
-                        continue
-                    is_integer = False
-                    if len(pattern_float.findall(w)) > 0:
-                        is_integer = (pattern_float.findall(w) == pattern_int.findall(w))
-                    # apply floating point regex
-                    for m in pattern_float.findall(w):
-                        index = line.index(m)
-                        # substitute dD by e
-                        m = pattern_d.sub('e', m)
-                        if is_integer:
-                            numbers.append(int(m))
-                        else:
-                            numbers.append(float(m))
-                        location.append((n, index, len(m)))
-        if f.use_mask:
-            if numbers == []:
-                raise FilterKeywordError('ERROR: mask %s did not extract any numbers\n' % f.mask)
-
-        return numbers, location
 
     def _underline(self, f, start_char, length, reference, number, is_integer):
         """
