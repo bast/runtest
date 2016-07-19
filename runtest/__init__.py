28a8de8c9d910e510c19688e95ceb9df186cd540
Radovan Bast <bast@users.noreply.github.com>
2016-07-19 19:16:04 +0200

    rearrange

--- runtest/__init__.py
+++ runtest/__init__.py
@@ -6,10 +6,12 @@ __author__ = ('Radovan Bast <radovan.bast@uit.no>')
 
 __version__ = '2.0.0-alpha-x'
 
-__all__ = ['Filter',
-           'TestRun',
-           'FilterKeywordError',
-           'TestFailedError',
-           'BadFilterError',
-           'AcceptedError',
-           'SubprocessError']
+__all__ = [
+    'Filter',
+    'TestRun',
+    'FilterKeywordError',
+    'TestFailedError',
+    'BadFilterError',
+    'AcceptedError',
+    'SubprocessError',
+]
