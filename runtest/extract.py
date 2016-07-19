ca13b0e64f4c526dd369869b755402f9892a2583
Radovan Bast <bast@users.noreply.github.com>
2016-07-19 14:19:32 +0200

    move tests

--- runtest/extract.py
+++ runtest/extract.py
@@ -53,3 +53,67 @@ def extract_numbers(f, text):
                     locations.append((n, index, len(m)))
 
     return numbers, locations
+
+
+def test_extract_numbers():
+    from .classes import Filter
+
+    text = '''<<A( 3),B( 3)>> - linear response function (real):
+-----------------------------------------------------------------------------------------------
+   A - Z-Dipole length      B1u  T+
+   B - Z-Dipole length      B1u  T+
+-----------------------------------------------------------------------------------------------
+ Frequency (real)     Real part                                     Convergence
+-----------------------------------------------------------------------------------------------
+  0.00000000 a.u.   -1.901357604797 a.u.                       3.04E-07   (converged)
+-----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------
+
+
+                         +--------------------------------+
+                         ! Electric dipole polarizability !
+                         +--------------------------------+
+
+
+ 1 a.u =   0.14818471 angstrom**3
+
+
+@   Elements of the electric dipole polarizability tensor
+
+@   xx            1.90135760 a.u.   (converged)
+@   yy            1.90135760 a.u.   (converged)
+@   zz            1.90135760 a.u.   (converged)
+
+@   average       1.90135760 a.u.
+@   anisotropy    0.000      a.u.
+
+@   xx            0.28175212 angstrom**3
+@   yy            0.28175212 angstrom**3
+@   zz            0.28175212 angstrom**3
+
+@   average       0.28175212 angstrom**3
+@   anisotropy    0.000      angstrom**3'''
+
+    f = Filter()
+    f.add()
+
+    numbers, locations = extract_numbers(f.filter_list[0], text.splitlines())
+
+    assert numbers == [0.0, -1.901357604797, 3.04e-07, 1, 0.14818471, 1.9013576, 1.9013576, 1.9013576, 1.9013576, 0.0, 0.28175212, 0.28175212, 0.28175212, 0.28175212, 0.0]
+    assert locations == [(7, 2, 10), (7, 20, 15), (7, 63, 8), (17, 1, 1), (17, 11, 10), (22, 18, 10), (23, 18, 10), (24, 18, 10), (26, 18, 10), (27, 18, 5), (29, 18, 10), (30, 18, 10), (31, 18, 10), (33, 18, 10), (34, 18, 5)]
+
+
+def test_extract_numbers_mask():
+    from .classes import Filter
+
+    text = '''1.0 2.0 3.0 4.0
+1.0 2.0 3.0 4.0
+1.0 2.0 3.0 4.0'''
+
+    f = Filter()
+    f.add(mask=[1, 4])
+
+    numbers, locations = extract_numbers(f.filter_list[0], text.splitlines())
+
+    assert numbers == [1.0, 4.0, 1.0, 4.0, 1.0, 4.0]
+    assert locations == [(0, 0, 3), (0, 12, 3), (1, 0, 3), (1, 12, 3), (2, 0, 3), (2, 12, 3)]
--- runtest/test/test.py
+++ runtest/test/test.py
@@ -5,76 +5,6 @@ import pytest
 from ..classes import Filter
 from ..exceptions import *
 
-# ------------------------------------------------------------------------------
-
-
-def test_extract_numbers():
-    from ..extract import extract_numbers
-
-    text = '''<<A( 3),B( 3)>> - linear response function (real):
------------------------------------------------------------------------------------------------
-   A - Z-Dipole length      B1u  T+
-   B - Z-Dipole length      B1u  T+
------------------------------------------------------------------------------------------------
- Frequency (real)     Real part                                     Convergence
------------------------------------------------------------------------------------------------
-  0.00000000 a.u.   -1.901357604797 a.u.                       3.04E-07   (converged)
------------------------------------------------------------------------------------------------
-----------------------------------------------------------------------------
-
-
-                         +--------------------------------+
-                         ! Electric dipole polarizability !
-                         +--------------------------------+
-
-
- 1 a.u =   0.14818471 angstrom**3
-
-
-@   Elements of the electric dipole polarizability tensor
-
-@   xx            1.90135760 a.u.   (converged)
-@   yy            1.90135760 a.u.   (converged)
-@   zz            1.90135760 a.u.   (converged)
-
-@   average       1.90135760 a.u.
-@   anisotropy    0.000      a.u.
-
-@   xx            0.28175212 angstrom**3
-@   yy            0.28175212 angstrom**3
-@   zz            0.28175212 angstrom**3
-
-@   average       0.28175212 angstrom**3
-@   anisotropy    0.000      angstrom**3'''
-
-    f = Filter()
-    f.add()
-
-    numbers, locations = extract_numbers(f.filter_list[0], text.splitlines())
-
-    assert numbers == [0.0, -1.901357604797, 3.04e-07, 1, 0.14818471, 1.9013576, 1.9013576, 1.9013576, 1.9013576, 0.0, 0.28175212, 0.28175212, 0.28175212, 0.28175212, 0.0]
-    assert locations == [(7, 2, 10), (7, 20, 15), (7, 63, 8), (17, 1, 1), (17, 11, 10), (22, 18, 10), (23, 18, 10), (24, 18, 10), (26, 18, 10), (27, 18, 5), (29, 18, 10), (30, 18, 10), (31, 18, 10), (33, 18, 10), (34, 18, 5)]
-
-# ------------------------------------------------------------------------------
-
-
-def test_extract_numbers_mask():
-    from ..extract import extract_numbers
-
-    text = '''1.0 2.0 3.0 4.0
-1.0 2.0 3.0 4.0
-1.0 2.0 3.0 4.0'''
-
-    f = Filter()
-    f.add(mask=[1, 4])
-
-    numbers, locations = extract_numbers(f.filter_list[0], text.splitlines())
-
-    assert numbers == [1.0, 4.0, 1.0, 4.0, 1.0, 4.0]
-    assert locations == [(0, 0, 3), (0, 12, 3), (1, 0, 3), (1, 12, 3), (2, 0, 3), (2, 12, 3)]
-
-# ------------------------------------------------------------------------------
-
 
 def test_filter_file():
     from ..main import _filter_file
@@ -101,8 +31,6 @@ raboof 1.0 3.0 7.0
 
     res = _filter_file(f=f.filter_list[0], file_name='raboof', output=text.splitlines())
     assert res == ['raboof 1.0 3.0 7.0', '       1.0 3.0 7.0', '       1.0 3.0 7.0', '       1.0 3.0 7.0', '       1.0 3.0 7.0']
-
-# ------------------------------------------------------------------------------
 
 
 def test_check():
@@ -152,8 +80,6 @@ ERROR           ### expected: 3.05 (abs diff: 5.00e-02)\n'''
 .       1.0 2.0 3.0
 ERROR           ### expected: 3.05 (abs diff: 5.00e-02 ignoring signs)\n'''
 
-# ------------------------------------------------------------------------------
-
 
 def test_check_bad_filter():
 
@@ -173,8 +99,6 @@ def test_check_bad_filter():
     with pytest.raises(BadFilterError) as e:
         f.check(work_dir='not used', out_name=out_name, ref_name=ref_name, verbose=False)
     assert 'ERROR: filter ["does not exist" ... "either"] did not extract anything from file %s\n' % out_name in str(e.value)
-
-# ------------------------------------------------------------------------------
 
 
 def test_check_different_length():
@@ -198,8 +122,6 @@ reference gave 3 numbers:
 1.0 2.0 3.05
 \n'''
 
-# ------------------------------------------------------------------------------
-
 
 def test_bad_keywords():
 
@@ -214,8 +136,6 @@ def test_bad_keywords():
     with pytest.raises(FilterKeywordError) as e:
         f.add(from_string='foo', from_re='foo', to_string='foo', to_re='foo')
     assert "ERROR: incompatible keyword pairs: [('from_re', 'from_string'), ('to_re', 'to_string')]\n" in str(e.value)
-
-# ------------------------------------------------------------------------------
 
 
 def test_only_string():
