240b39c9377463f4eed88328c654274801ae6ff5
Radovan Bast <bast@users.noreply.github.com>
2016-07-21 14:22:32 +0200

    add API to version information

--- runtest/__init__.py
+++ runtest/__init__.py
@@ -1,12 +1,13 @@
 from .ugly import get_filter, run
+from .version import version_info, __version__
 from .cli import cli
 
 __author__ = ('Radovan Bast <radovan.bast@uit.no>')
 
-__version__ = '2.0.0-alpha-x'
-
 __all__ = [
     'get_filter',
+    '__version__',
+    'version_info',
     'run',
     'cli',
 ]
