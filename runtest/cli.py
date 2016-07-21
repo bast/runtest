356b4bf9e0eee9176b1cd5201e8a437d4c0f7c32
Radovan Bast <bast@users.noreply.github.com>
2016-07-21 14:30:40 +0200

    nicer import

--- runtest/cli.py
+++ runtest/cli.py
@@ -1,17 +1,17 @@
 def cli():
 
     from optparse import OptionParser
-    from runtest import __version__
     import sys
     import os
     import inspect
+    from .version import __version__ as _version
 
     frame = inspect.stack()[-1]
     module = inspect.getmodule(frame[0])
     caller_file = module.__file__
     caller_dir = os.path.dirname(os.path.realpath(caller_file))
 
-    parser = OptionParser(description='runtest {0} - Numerically tolerant test library.'.format(__version__))
+    parser = OptionParser(description='runtest {0} - Numerically tolerant test library.'.format(_version))
 
     parser.add_option('--binary-dir',
                       '-b',
