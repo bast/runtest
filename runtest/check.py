4530c4003df4cc1da23fc0b50b38258e8f16a281
Radovan Bast <bast@users.noreply.github.com>
2016-07-22 09:40:51 +0200

    move tests

--- runtest/check.py
+++ runtest/check.py
@@ -130,3 +130,139 @@ def check(filter_list, out_name, ref_name, verbose=False):
         if verbose:
             message += diff
         raise TestFailedError(message)
+
+
+def test_check():
+    import os
+    import pytest
+    from .exceptions import FilterKeywordError, TestFailedError
+    from .check import check
+    from .filter_constructor import get_filter
+
+    HERE = os.path.abspath(os.path.dirname(__file__))
+
+    out_name = os.path.join(HERE, 'test', 'out.txt')
+    ref_name = os.path.join(HERE, 'test', 'ref.txt')
+
+    filters = [get_filter(abs_tolerance=0.1)]
+    check(filters, out_name=out_name, ref_name=ref_name, verbose=False)
+
+    filters = [get_filter()]
+    with pytest.raises(FilterKeywordError) as e:
+        check(filters, out_name=out_name, ref_name=ref_name, verbose=False)
+    assert 'ERROR: for floats you have to specify either rel_tolerance or abs_tolerance\n' in str(e.value)
+
+    filters = [get_filter(rel_tolerance=0.01)]
+    with pytest.raises(TestFailedError) as e:
+        check(filters, out_name=out_name, ref_name=ref_name, verbose=False)
+    assert 'ERROR: test %s failed\n' % out_name in str(e.value)
+    with open(os.path.join(HERE, 'test', 'out.txt.diff'), 'r') as f:
+        assert f.read() == '''
+.       1.0 2.0 3.0
+ERROR           ### expected: 3.05 (rel diff: 1.64e-02)\n'''
+
+    filters = [get_filter(abs_tolerance=0.01)]
+    with pytest.raises(TestFailedError) as e:
+        check(filters, out_name=out_name, ref_name=ref_name, verbose=False)
+    assert 'ERROR: test %s failed\n' % out_name in str(e.value)
+    with open(os.path.join(HERE, 'test', 'out.txt.diff'), 'r') as f:
+        assert f.read() == '''
+.       1.0 2.0 3.0
+ERROR           ### expected: 3.05 (abs diff: 5.00e-02)\n'''
+
+    filters = [get_filter(abs_tolerance=0.01, ignore_sign=True)]
+    with pytest.raises(TestFailedError) as e:
+        check(filters, out_name=out_name, ref_name=ref_name, verbose=False)
+    assert 'ERROR: test %s failed\n' % out_name in str(e.value)
+    with open(os.path.join(HERE, 'test', 'out.txt.diff'), 'r') as f:
+        assert f.read() == '''
+.       1.0 2.0 3.0
+ERROR           ### expected: 3.05 (abs diff: 5.00e-02 ignoring signs)\n'''
+
+
+def test_check_bad_filter():
+    import os
+    import pytest
+    from .exceptions import BadFilterError
+    from .check import check
+    from .filter_constructor import get_filter
+
+    HERE = os.path.abspath(os.path.dirname(__file__))
+
+    out_name = os.path.join(HERE, 'test', 'out.txt')
+    ref_name = os.path.join(HERE, 'test', 'ref.txt')
+
+    filters = [get_filter(from_string='does not exist', num_lines=4)]
+    with pytest.raises(BadFilterError) as e:
+        check(filters, out_name=out_name, ref_name=ref_name, verbose=False)
+    assert 'ERROR: filter [4 lines from "does not exist"] did not extract anything from file %s\n' % out_name in str(e.value)
+
+    filters = [get_filter(from_string='does not exist', to_string="either")]
+    with pytest.raises(BadFilterError) as e:
+        check(filters, out_name=out_name, ref_name=ref_name, verbose=False)
+    assert 'ERROR: filter ["does not exist" ... "either"] did not extract anything from file %s\n' % out_name in str(e.value)
+
+
+def test_check_different_length():
+    import os
+    import pytest
+    from .exceptions import TestFailedError
+    from .check import check
+    from .filter_constructor import get_filter
+
+    HERE = os.path.abspath(os.path.dirname(__file__))
+
+    out_name = os.path.join(HERE, 'test', 'out2.txt')
+    ref_name = os.path.join(HERE, 'test', 'ref.txt')
+
+    filters = [get_filter(abs_tolerance=0.1)]
+    with pytest.raises(TestFailedError) as e:
+        check(filters, out_name=out_name, ref_name=ref_name, verbose=False)
+    assert 'ERROR: test %s failed\n' % out_name in str(e.value)
+    with open(os.path.join(HERE, 'test', 'out2.txt.diff'), 'r') as f:
+        assert f.read() == '''ERROR: extracted sizes do not match
+own gave 4 numbers:
+1.0 2.0 3.0 4.0
+
+reference gave 3 numbers:
+1.0 2.0 3.05
+\n'''
+
+
+def test_bad_keywords():
+    import os
+    import pytest
+    from .filter_api import recognized_kw
+    from .exceptions import FilterKeywordError
+    from .filter_constructor import get_filter
+
+    with pytest.raises(FilterKeywordError) as e:
+        _ = get_filter(raboof=0, foo=1)
+    exception = '''ERROR: keyword(s) (foo, raboof) not recognized
+       available keywords: ({0})\n'''.format(', '.join(recognized_kw))
+    assert exception in str(e.value)
+
+    with pytest.raises(FilterKeywordError) as e:
+        _ = get_filter(from_string='foo', from_re='foo', to_string='foo', to_re='foo')
+    assert "ERROR: incompatible keyword pairs: [('from_re', 'from_string'), ('to_re', 'to_string')]\n" in str(e.value)
+
+
+def test_only_string():
+    import os
+    import pytest
+    from .exceptions import BadFilterError
+    from .check import check
+    from .filter_constructor import get_filter
+
+    HERE = os.path.abspath(os.path.dirname(__file__))
+
+    out_name = os.path.join(HERE, 'test', 'only_string_out.txt')
+    ref_name = os.path.join(HERE, 'test', 'only_string_ref.txt')
+
+    filters = [get_filter(string='raboof')]
+    check(filters, out_name=out_name, ref_name=ref_name, verbose=False)
+
+    filters = [get_filter(string='foo')]
+    with pytest.raises(BadFilterError) as e:
+        check(filters, out_name=out_name, ref_name=ref_name, verbose=False)
+    assert 'ERROR: filter [1 lines from "foo"] did not extract anything from file %s\n' % out_name in str(e.value)
